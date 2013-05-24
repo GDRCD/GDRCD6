3f601600d6b9d4bbfecbc71d5e097c7283df057a
Stefano Campanella <stefano.campanella@globalcom.tv.it>
2013-05-25 00:41:48 +0200

    Aggiunta un eccezione per il database

---
+++ core/driver/db.exception.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Definisce l'eccezione da lanciare per le operazioni del DB, in modo da poterle distinguere da
+ * altre eccezioni nei blocchi catch
+ */
+class DBException extends Exception
+{
+    /**
+     * Per ora non implementa niente di diverso da Exception.
+     */
+}
