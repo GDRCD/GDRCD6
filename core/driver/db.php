3eb34954ec28dad25af53c34ea6eb597b8b8bc94
Stefano Campanella <stefano.campanella@globalcom.tv.it>
2013-05-25 00:52:27 +0200

    Aggiunta la classe loader del driver, componente principale per interfacciarsi con il layer del db

---
+++ core/driver/db.php
@@ -0,0 +1,52 @@
+<?php
+require_once(dirname(__FILE__) . GDRCD_DS . 'dbdriver.interface.php');
+require_once(dirname(__FILE__) . GDRCD_DS . 'db.exception.php');
+
+/**
+ * Rappresenta l'interfaccia principale per accedere layer di astrazione di GDRCD
+ * Si occupa di scegliere il driver corretto del database e di inviargli le query da eseguire.
+ * Questa è una classe completamente statica, utile semplicemente per la selezione del driver
+ * e per poter accedere al database con un nome unico, non legato al nome del driver.
+ */
+class DB
+{
+    /**
+     * The database driver object
+     */
+    static private $dbObj;
+    
+    /**
+     * Classe statica, non è pensata per venire istanziata
+     */
+    private function __construct(){}
+    
+    /**
+     * Crea la connessione con il database
+     * @param $driver: la classe driver da usare per la connessione al db, deve implementare 
+     * l'interfaccia DatabaseDriver
+     * @see DatabaseDriver::__construct()
+     * @throws DBException in caso di fallimento della connessione
+     * 
+     * TODO considerare se è il caso di non usare $driver e leggere direttamente da GDRCD_DATABASE_DRIVER
+     */
+    static public function connect($driver, $host, $user, $passwd, $dbName)
+    {
+        $driver_file=dirname(__FILE__) . GDRCD_DS . 'driver.' . strtolower($driver) . '.php';
+        if(file_exists($driver_file)) {
+            require_once($driver_file);
+            self::$dbObj=new $driver($host,$user,$passwd,$dbName);
+        }
+    }
+    
+    /**
+     * Metodo interno per PHP, usato per reindirizzare le chiamate al driver del DB
+     */
+    public function __callStatic($method, $params = null)
+    {
+        if(method_exists(self::$dbObj, $method)){
+            return call_user_func_array(array(self::$dbObj,$method), $params);
+        }else{
+            throw new DBException("Il metodo ".$method." non esiste!");
+        }
+    }
+}
